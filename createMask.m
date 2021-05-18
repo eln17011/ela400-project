45762086c1fb13aa24a31fb0aa0354625c7bfbed
eln17011 <31732187+eln17011@users.noreply.github.com>
2021-05-18 12:55:26 +0200

    added more to main

---
+++ createMask.m
@@ -0,0 +1,8 @@
+function mask = createMask(left,right)
+
+    %något med alla options
+
+    mask = disparitySGM(left,right);
+
+end
+
--- main.m
+++ main.m
@@ -1,9 +1,34 @@
+clear;clc;close all;
+
 webcameras = webcamlist;
 
 if isempty(webcameras)
-    msgbox('Please connect your webcameras')
+    msgbox('Please connect all 3 webcameras')  
 end
 
-for i = 
-options
+if size(webcameras,1) == 3
+  tic  
+    while toc < 10
+        mainfeed = webcam(char(webcameras(1)));
+        left = webcam(char(webcameras(2)));
+        right = webcam(char(webcameras(3)));
+
+        liveimg = snapshot(mainfeed);
+        leftimg = rgb2gray(snapshot(left));
+        rightimg = rgb2gray(snapshot(right));
+
+        mask = createMask(leftimg,rightimg);
+
+        final = immultiply(liveimg,mask);
+        
+        imshow(final);
+    end
+else
+    msgbox('Please connect all 3 webcameras!') 
+end
+
+
+
+
+
 
